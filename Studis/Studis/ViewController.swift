0670ad581cc0e34281c7e6d0438e9d1fef943633
Victor Melo <victor.contas@outlook.com>
2018-03-01 17:04:08 -0300

    Project Creation

---
+++ Studis/Studis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Studis
+//
+//  Created by Victor S Melo on 01/03/18.
+//  Copyright © 2018 Victor S Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
